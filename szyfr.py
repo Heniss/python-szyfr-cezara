5aeb3378086fa2e9a9c80d0f236c558b7ecdd9f8
Henio Szewczyk <66276627+Heniss@users.noreply.github.com>
2023-06-10 12:43:45 +0200

    Update szyfr.py

--- szyfr.py
+++ szyfr.py
@@ -1,6 +1,6 @@
-slownik = {"a": "x", "b": "y", "c": "z", "d": "a", "e": "b", "f": "c", "g": "d", "h": "e", "i": "f", #slownik przypisujący każdej literze odpowiednią wartość zgodnie z szyfrenm cezara
+slownik = {"a": "x", "b": "y", "c": "z", "d": "a", "e": "b", "f": "c", "g": "d", "h": "e", "i": "f", 
            "j": "g", "k": "h", "l": "i", "m": "j", "n": "k", "o": "l", "p": "m", "q": "n", "r": "o",
-           "s": "p", "t": "q", "u": "r", "v": "s", "w": "t", "x": "u", "y": "v", "z": "w"}
+           "s": "p", "t": "q", "u": "r", "v": "s", "w": "t", "x": "u", "y": "v", "z": "w"} #slownik przypisujący każdej literze odpowiednią wartość zgodnie z szyfrenm cezara
 
 while True: #pętla działająca ciągle
     slowo = str(input("Podaj slowo do zaszyfrowania: ")) #podanie słowa do zaszyfrowania
